a38e47c0556f756fd5ad2d33e9e7a285ebcc75d9
Mike-Mp <devmichalis@protonmail.com>
2025-10-18 00:08:25 +0300

    move to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mikemp.gr',
+  site: 'https://Mike-Mp.github.io',
   integrations: [mdx(), sitemap(), tailwind()]
 });
